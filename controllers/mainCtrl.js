6a8e1285d59de82d0cbbf329623aea8392b41bdf
linoyzaga <linoyzaga@gmail.com>
2016-08-01 18:19:29 +0300

    Last view Updates

--- controllers/mainCtrl.js
+++ controllers/mainCtrl.js
@@ -1,5 +1,9 @@
-TripApp.controller('mainCtrl', ['$scope', function($scope) {
-    
+TripApp.controller('mainCtrl', ['$scope', function ($scope) {
+
+    // Scrolling the page up
+    window.scrollTo(0, 0);
+
+    // Carousel movment
     $('.carousel').carousel({
         interval: 5000 //changes the speed
     })
--- controllers/sitesCtrl.js
+++ controllers/sitesCtrl.js
@@ -1,8 +1,11 @@
 TripApp.controller('sitesCtrl', ['$scope', function ($scope) {
 
+    // Scrolling the page up
+    window.scrollTo(0, 0);
+
     // Hide the side bar
     $scope.hideSideNav = function () {
-                $("#wrapper").toggleClass("toggled");
-        };
+        $("#wrapper").toggleClass("toggled");
+    };
 
 }]);

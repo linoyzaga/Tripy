963de8250b432584caea327aa952d9fa2eacc4eb
linoyzaga <linoyzaga@gmail.com>
2016-08-13 16:19:21 +0300

    Adding the server changes 13.8

--- server.js
+++ server.js
@@ -4,53 +4,34 @@
 var express = require('express');
 var app = express();
 var bodyparser = require("body-parser");
+var mongojs = require("mongojs");
 
 // configuration ======================================================================================================
-var MongoClient = require('mongodb').MongoClient;
-var assert = require('assert');
-var url = 'mongodb://ds139725.mlab.com:39725/tripydb';
-
-// Use connect method to connect to the Server
-MongoClient.connect(url, function(err, db) {
-    assert.equal(null, err);
-
-    // Check the connections completed
-    if (err) {
-        console.log('Unable to connect to the mongoDB server. Error:', err);
-    }
-    else {
-        console.log('Connection established to', url);
-
-        // Function ===================================================================================================
-
-        // Get all the locations function
-        app.get('/sites', function (req, res) {
-
-            console.log("Get all locations function node received");
-
-            db.collection('Locations').find(function (err, docs) {
-
-                if (err) {
-                    console.log(err);
-                    return res(err);
-                }
-                else {
-
-                    console.log(docs.toArray());
-                    return res.send(docs.toArray());
-
-                    // return res.json(docs);
-                }
-            })
-        })
-
-        // Close the connections
-        db.close();
-    }
-});
-
+var db = mongojs('mongodb://admin:123456@ds139725.mlab.com:39725/tripydb', ['Locations']);
 app.use(express.static(__dirname + "/public"));
 app.use(bodyparser.json());
+
+// Function ===========================================================================================================
+
+// Get all locations
+app.get('/sites', function(req, res) {
+    console.log("Get all locations function node received");
+
+    db.Locations.find(function (err, docs) {
+
+        // Check the connection completed
+        if (err) {
+
+            console.log('Unable to connect to the mongoDB server. Error:', err);
+        }
+        else {
+            console.log(docs);
+
+            // Return value
+            return res.json(docs);
+        }
+    });
+});
 
 // Loading ============================================================================================================
 app.listen(8080);
